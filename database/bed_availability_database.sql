7f34cd652f5c3db83b861a9df861cbec1df95077
Miguel Carlo Fernandez <mcfernandez8@up.edu.ph>
2021-03-23 19:36:57 +0800

    Created database structure

--- database/bed_availability_database.sql
+++ database/bed_availability_database.sql
@@ -1,13 +1,22 @@
 USE bedability;
-CREATE TABLE hospital(
-	hospital_id int,
+
+CREATE TABLE personnel(
+	personnel_id int,
     name varchar(255),
-    address varchar(255),
-    available_beds int
+    email varchar(255),
+    position varchar(255),
+    is_master bool,
+    username varchar(255),
+    password varchar(255),
+    hospital_id int,
+    PRIMARY KEY (personnel_id),
+    FOREIGN KEY (hospital_id) REFERENCES hospital(hospital_id)
 );
 
-CREATE TABLE patient(
-	patient_id int,
-    name varchar(255),
-    address varchar(255)
+CREATE TABLE request(
+	request_no int,
+    FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
+    FOREIGN KEY (hospital_id) REFERENCES hospital(hospital_id),
+    date_requested datetime,
+    PRIMARY KEY (request_no)
 );
