477e531fd6e2f7914d8e6df37444b0311e072c96
Miguel Carlo Fernandez <mcfernandez8@up.edu.ph>
2021-04-21 16:41:39 +0800

    Transferred UI files in resources/views

--- project/routes/web.php
+++ project/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/patient-registration', function () {
+    return view('Patient Registration UI.index');
+});
