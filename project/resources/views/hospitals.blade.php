ed9d70e2e8a2079e8c7b08942e72cd64b8162a60
Bon Gibalay <blgibalay@up.edu.ph>
2021-04-28 12:30:32 +0800

    added the hospitals view

--- project/resources/views/hospitals.blade.php
+++ project/resources/views/hospitals.blade.php
@@ -4,7 +4,10 @@
 <div class="flex-center position-ref full-height">
     <div class="content">
         <div class="title m-b-md">
-            Just a test test 
+            Imagine this a hospital list 
+            <p> hospital 1 </p>
+            <p> hospital 2 </p>
+            <p> hospital 3 </p>
         </div>
     </div>
 </div>
--- project/routes/web.php
+++ project/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return view('test');
 });
+
+Route::get('/hospitals', function () {
+    return view('hospitals');
+});
