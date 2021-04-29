1cdb7f234da583fab910d225f13956e83cfe7c89
Miguel Carlo Fernandez <mcfernandez8@up.edu.ph>
2021-04-29 12:09:45 +0800

    added app.blade.php

--- project/app/Http/Controllers/PatientReceiverController.php
+++ project/app/Http/Controllers/PatientReceiverController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class PatientReceiverController extends Controller
 {
-    //
+    public function ReceiveIt(Request $request){
+        $validatedData = $request->validate([
+            'id' => 'nullable',
+            'email' => 'nullable',
+            'first_name' => 'nullable',
+            'last_name' => 'nullable',
+            'date_of_birth' => 'nullable',
+            'address' => 'nullable',
+            'contact_number' => 'nullable',
+            'email_verified_at' => 'nullable',
+            'password' => 'nullable',
+            'remember' => 'nullable',
+            'timestamps' => 'nullable'
+        ]);
+
+        return json_encode($validatedData);
+    }
 }
